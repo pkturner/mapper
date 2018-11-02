d67b417d7eb6c30bd713426aba542448a3130fe3
Kai Pastor <dg0yt@darc.de>
2018-11-02 06:21:05 +0100

    Track: Remove support for non-geographic coords
    DXF support was removed, and GPX tracks use geographic coordinates
    (WGS84).
    This change introduces (or uncovers) the mapper-gdal libs dependency
    on linking Mapper_Common, caused by OgrTemplate accessing Track via
    TemplateTrack.

--- src/core/track.h
+++ src/core/track.h
@@ -81,8 +81,8 @@ inline bool operator!=(const TrackPoint& lhs, const TrackPoint& rhs) { return !(
 
 /**
  * Stores a set of tracks and / or waypoints, e.g. taken from a GPS device.
- * Can optionally store a track coordinate reference system in track_georef;
- * if no track CRS is given, assumes that coordinates are geographic WGS84 coordinates
+ * 
+ * All coordinates are assumed to be geographic WGS84 coordinates.
  */
 class Track
 {
@@ -131,10 +131,6 @@ public:
 	/** Updates the map positions of all points based on the new georeferencing. */
 	void changeMapGeoreferencing(const Georeferencing& new_georef);
 	
-	/// Sets the track coordinate reference system.
-	/// The Track object takes ownership of the Georeferencing object.
-	void setTrackCRS(Georeferencing* track_crs);
-	
 	// Getters
 	int getNumSegments() const;
 	int getSegmentPointCount(int segment_number) const;
@@ -143,9 +139,6 @@ public:
 	int getNumWaypoints() const;
 	const TrackPoint& getWaypoint(int number) const;
 	const QString& getWaypointName(int number) const;
-	
-	bool hasTrackCRS() const {return track_crs;}
-	Georeferencing* getTrackCRS() const {return track_crs;}
 	
 	/// Averages all track coordinates
 	LatLon calcAveragePosition() const;
@@ -168,7 +161,6 @@ private:
 	
 	bool current_segment_finished;
 	
-	Georeferencing* track_crs;
 	Georeferencing map_georef;
 	
 	friend bool operator==(const Track& lhs, const Track& rhs);
@@ -177,10 +169,10 @@ private:
 /**
  * Compares waypoints, segments, and track points for equality.
  * 
- * This operator does not (explicitly) compare the tracks' data CRS or map
- * georeferencing. When these features are actually used, they affect the
- * projection to map coordinates, and so their effects are recorded in the
- * waypoints and track points.
+ * This operator does not (explicitly) compare the tracks' map georeferencing.
+ * When this feature is actually used, it affects the projection to map
+ * coordinates, and so its effects are recorded in the waypoints and track
+ * points.
  */
 bool operator==(const Track& lhs, const Track& rhs);
 
